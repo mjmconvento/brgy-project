019ed5ead9641ea6e9c478ca684ca2a690e585c1
mj <mjmconvento@gmail.com>
2017-07-12 02:18:30 +0800

    reverted back public

--- resources/views/Templates/register_css.blade.php
+++ resources/views/Templates/register_css.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="{!! asset('public/template_libraries/bootstrap/css/bootstrap.min.css') !!}">
+<link rel="stylesheet" href="{!! asset('template_libraries/bootstrap/css/bootstrap.min.css') !!}">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="{!! asset('public/template_libraries/dist/css/AdminLTE.min.css') !!}">
-<link rel="stylesheet" href="{!! asset('public/template_libraries/plugins/iCheck/square/blue.css') !!}">
+<link rel="stylesheet" href="{!! asset('template_libraries/dist/css/AdminLTE.min.css') !!}">
+<link rel="stylesheet" href="{!! asset('template_libraries/plugins/iCheck/square/blue.css') !!}">
